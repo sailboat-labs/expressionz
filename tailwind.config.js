3aa094016184e15108ab3cd3b03d09f117e2dbfc
lorrainemakuyana <lorrainemakuyana23@gmail.com>
2024-03-21 15:36:16 +0200

    feat: added tailwind class formatter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkPurple: "#262630",
+        yellow: "#FFD702",
+        darkGrey: "#30303C",
+      },
+    },
   },
   plugins: [],
 };
-

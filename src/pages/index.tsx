5d788e7ad4b982937a55c36f23e5dedccd88dfd7
lorrainemakuyana <lorrainemakuyana23@gmail.com>
2024-03-21 16:55:05 +0200

    banner added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 import TokenMarquee from "@/components/home/Marquee";
+import Banner from "@/components/home/Banner";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-screen">
       <Header />
       <TokenMarquee />
+      <Banner />
 
       <Footer />
     </main>

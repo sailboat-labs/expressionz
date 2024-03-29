20a32fb1964ab6f116e0b7781f9f519cb12e4dfc
lorrainemakuyana <lorrainemakuyana23@gmail.com>
2024-03-29 15:04:03 +0200

    feat: added moonbirds page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Image from "next/image";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          type="module"
+          src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+        ></script>
+        <script
+          noModule
+          src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />

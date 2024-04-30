eeeed6ce28919b4f96e7d71df0259f134f0d8053
Obitrim <pevangelist516@gmail.com>
2024-04-30 09:45:16 +0000

    style: improve layout and alignment

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -9,31 +9,32 @@ const CollectionsPage = () => {
     <>
       <Seo title="Collections" />
       <BaseLayout>
-        <h1
-          className={cn(
-            "pb-16 pt-8 text-center text-3xl text-yellow md:my-10 md:text-5xl",
-          )}
-        >
-          Collections
-        </h1>
-        <section>
+        <img
+          className=" mx-auto my-16 max-w-sm lg:max-w-lg"
+          src="/images/collections-heading.webp"
+          alt="text 'collection'"
+        />
+        <section className="mb-16">
           <div
             className={cn(
               "mx-auto grid max-w-md place-content-center gap-8 sm:flex sm:grid-cols-2",
               "md:max-w-7xl md:gap-20",
             )}
           >
-            <motion.div className="w-fit" whileHover={{ scale: 1.15 }}>
+            <motion.div
+              className="w-fit cursor-pointer"
+              whileHover={{ scale: 1.15 }}
+            >
               <Link
                 className={cn(
                   "flex flex-col items-center",
-                  "max-w-[300px]  border-2 border-yellow p-4 pb-0",
+                  "max-w-[300px] cursor-pointer  border-4 border-yellow p-4 pb-0",
                 )}
                 href="/collections/moonbirds"
               >
                 <strong
                   className={cn(
-                    "font-presstart translate-y-4 uppercase text-yellow",
+                    "font-presstart translate-y-4 text-lg uppercase text-yellow",
                   )}
                 >
                   Moonbirds
@@ -45,15 +46,20 @@ const CollectionsPage = () => {
                 />
               </Link>
             </motion.div>
-            <motion.div className="w-fit" whileHover={{ scale: 1.15 }}>
+            <motion.div
+              className="w-fit cursor-pointer"
+              whileHover={{ scale: 1.15 }}
+            >
               <Link
                 className={cn(
                   "flex flex-col items-center",
-                  "h-full max-w-[300px] border-2 border-yellow px-4 pb-0 pt-8",
+                  "h-full max-w-[300px] cursor-pointer border-4 border-yellow px-4 pb-0 pt-8",
                 )}
                 href="/collections/wizards"
               >
-                <strong className={cn("font-presstart uppercase text-yellow")}>
+                <strong
+                  className={cn("font-presstart text-lg uppercase text-yellow")}
+                >
                   Wizards
                 </strong>
                 <img

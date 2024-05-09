05a73b8b5e815256ffce69dd838a07f9d806eef4
Obitrim <pevangelist516@gmail.com>
2024-05-09 08:13:56 +0000

    feat: improve seo

--- src/components/home/StartJourney.tsx
+++ src/components/home/StartJourney.tsx
@@ -7,28 +7,34 @@ function StartJourney() {
   return (
     <>
       <ContactModal show={showContactModal} setShow={setShowContactModal} />
-      <section className="font-presstart w-full space-y-10 bg-[#E5E5E5] py-12 lg:space-y-7 lg:py-16 3xl:space-y-10 3xl:py-24">
+      <section className="w-full space-y-10 bg-[#E5E5E5] py-12 font-presstart lg:space-y-7 lg:py-16 3xl:space-y-10 3xl:py-24">
         <p className="text-center text-2xl uppercase text-[#2E2E40] lg:text-3xl 3xl:text-4xl">
           Start your journey as a:
         </p>
 
         <div className="flex flex-col items-center justify-center space-y-5 lg:flex-row lg:space-x-8 lg:space-y-0 3xl:space-x-14">
-          <img
-            src="/images/buttons/collector.webp"
-            className="w-48 cursor-pointer 3xl:w-80"
-            onClick={() => setShowContactModal(true)}
-          />
+          <button onClick={() => setShowContactModal(true)}>
+            <img
+              src="/images/buttons/collector.webp"
+              className="w-48 cursor-pointer 3xl:w-80"
+              alt="button with 'collector' text"
+            />
+          </button>
 
-          <img
-            src="/images/buttons/project-team.webp"
-            className="w-48 cursor-pointer 3xl:w-80"
-            onClick={() => setShowContactModal(true)}
-          />
+          <button onClick={() => setShowContactModal(true)}>
+            <img
+              src="/images/buttons/project-team.webp"
+              className="w-48 cursor-pointer 3xl:w-80"
+              onClick={() => setShowContactModal(true)}
+              alt="button with 'project-team' text"
+            />
+          </button>
 
           <img
             src="/images/buttons/community-group.webp"
             className="w-48 cursor-pointer 3xl:w-80"
             onClick={() => setShowContactModal(true)}
+            alt="button with 'community group' text"
           />
         </div>
       </section>

0e3e26291a875b87e9ebc4b2e86c05420a1c552b
lorrainemakuyana <lorrainemakuyana23@gmail.com>
2024-05-15 15:09:48 +0200

    fix: matched emojis for wizards

--- src/data/emoji.data.ts
+++ src/data/emoji.data.ts
@@ -34,13 +34,14 @@ export const emojiMap = {
 
 export const wizardEmojis = [
   { emoji_type: "heart_eyes", type: "gif" },
-  { emoji_type: "umbrella", type: "gif" },
+  { emoji_type: "heart_eyes", type: "gif" },
+  // { emoji_type: 'umbrella', type: 'gif' },
   { emoji_type: "lfg", type: "gif" },
   { emoji_type: "sigh", type: "gif" },
   { emoji_type: "tea_cup", type: "png" },
-  { emoji_type: "smiley_face", type: "png" },
+  // { emoji_type: 'smiley_face', type: 'png' },
   { emoji_type: "sleeping", type: "png" },
-  { emoji_type: "tears_of_joy", type: "png" },
+  // { emoji_type: 'tears_of_joy', type: 'png' },
   { emoji_type: "side_eye", type: "png" },
   { emoji_type: "crying", type: "png" },
   { emoji_type: "enraged_face", type: "png" },

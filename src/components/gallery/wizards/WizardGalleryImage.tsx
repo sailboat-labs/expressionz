9a37570127bd85b3cdb81ba2f5f9157c65a52e10
Francis Eshun <eshunfrancisboateng@gmail.com>
2025-07-29 11:15:34 +0000

    refactor: reorganize imports in WizardGalleryImage component

--- src/components/gallery/wizards/WizardGalleryImage.tsx
+++ src/components/gallery/wizards/WizardGalleryImage.tsx
@@ -1,15 +1,15 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { toast } from "sonner";
-import { motion } from "framer-motion";
 
 import { IoCopyOutline } from "react-icons/io5";
 
-import { GALLERY } from "@/data/gallery";
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
-import { cn } from "@/lib/misc.lib";
 import ScrollTokenFrame from "@/components/shared/ScrollTokenFrame";
+import { GALLERY } from "@/data/gallery";
+import { cn } from "@/lib/misc.lib";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 export default function GalleryImage({
   wizard,
